aa6a9f942d37d3d76d2826260d70ce8fa07579a5
unknown <kalessin.s@gmail.com>
2023-12-14 17:21:03 +0200

    14-auth-5 user list done

--- src/class/user.js
+++ src/class/user.js
@@ -49,6 +49,14 @@ class User {
       ) || null
     )
   }
+
+  static getById(id) {
+    return (
+      this.#list.find((user) => user.id === Number(id)) || null
+    )
+  }
+
+  static getList = () => this.#list
 }
 
 module.exports = {
--- src/route/auth.js
+++ src/route/auth.js
@@ -8,9 +8,21 @@ const { Confirm } = require('../class/confirm')
 const { Session } = require('../class/session')
 
 User.create({
-  email: 'test@mail.com',
+  email: 'user@mail.com',
   password: 12345,
   role: 1,
+})
+
+User.create({
+  email: 'admin@mail.com',
+  password: 12345,
+  role: 2,
+})
+
+User.create({
+  email: 'devops@mail.com',
+  password: 12345,
+  role: 3,
 })
 
 // ================================================================
